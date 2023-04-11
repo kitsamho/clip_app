ee67f406ba325144634adfeeb154bb6f9bb3490b
kitsamho <kitsamho@gmail.com>
2023-04-11 10:11:42 +0100

    pushing data

--- clip_app.py
+++ clip_app.py
@@ -30,7 +30,7 @@ df_cached_embeddings_captions = load_cached_embeddings(cached_embedding_path_con
 # Define menu items for each sub-app
 navigation_buttons = {"Zero Shot Image Classification": image_classification,
                       "Zero Shot Text Classification": text_classification,
-                      "Image Clustering": image_clustering,
+                      # "Image Clustering": image_clustering,
                       "Text Clustering": text_clustering,
                       "Semantic Search": semantic_search,
                       "Appendix": data}
@@ -46,8 +46,8 @@ if selection == 'Zero Shot Image Classification':
 elif selection == 'Zero Shot Text Classification':
     page.write(tokeniser, model)
 
-elif selection == 'Image Clustering':
-    page.write(df_cached_embeddings_rock, processor, model)
+# elif selection == 'Image Clustering':
+#     page.write(df_cached_embeddings_rock, processor, model)
 
 elif selection == 'Text Clustering':
     page.write(df_cached_embeddings_captions, tokeniser, model)
